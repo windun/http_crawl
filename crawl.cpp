25208b5e40d8d43258c27ee37bf425f7743ca1b8
laterDays <dadamszx@gmail.com>
2014-11-28 13:36:39 -0500

    working parser

--- crawl.cpp
+++ crawl.cpp
@@ -2,41 +2,22 @@
 	Compile with: g++ crawl.cpp -o crawl -lcurl -std=c++11
 
 */
-#include <iostream>
-#include <stdio.h>
-#include <cstdio>
-#include <cstring>		// memset ()
-#include <string>
-#include <list>
-#include <stack>
-#include <unordered_map>
-#include <unordered_set>
+#include <cstring>			// memcpy () - aggregating data received by CURLOPT_WRITEFUNCTION (curl.h)
+#include <unordered_map>	// for URLS - data structure to track all urls, their source and destinations
+#include <unordered_set>	// for URLS
 #include <curl/curl.h>		// http gathering
+#include <regex.h> 			// link recognition (vs. file)
+#include "Parser.h"
 
-// link recognition (vs. file)
-#include <sys/types.h>
-#include <regex.h> 
+#define REGEX_PATTERN "[a-zA-Z0-9./]*(.bmp|.gif|.jpg|.pdf|.png)"	// link recognition - regex.h
+#define DATA_DIR "data/"		// fwrite () into this directory
+#define INDENT_STEP 5			// print_column () - indent columns
+#define COLUMN_WIDTH 40			// print_column () - width of block to print
+#define MAX_BUF_SIZE 1000000	// for UrlData buffer (stores html data)
+#define MSG_BUF_SIZE 4096		// link recognition (regex.h) error
+#define MAX_CURL_DEPTH 2		// limit depth of data gathering
 
-#define REGEX_PATTERN "[a-zA-Z0-9./]*(.bmp|.gif|.jpg|.pdf|.png)"
-#define DATA_DIR "data/"
-#define INDENT_STEP 5
-#define COLUMN_WIDTH 40
-#define MAX_BUF_SIZE 1000000
-#define MSG_BUF_SIZE 4096
-#define MAX_CURL_DEPTH 1
-
-
-// unordered_map
-// target url
-//	unordered_set
-//	<- origin url
-//	<- origin url
-// target url //	unordered_set
-//	<- origin url
-//	...
-//std::unordered_multimap<std::string, std::string> URLS;
 std::unordered_map<std::string, std::unordered_set<std::string>*> URLS;
-
 struct UrlData
 {
 	std::string source;
@@ -122,333 +103,6 @@ std::string toString (const char *c_str)
 	return str;
 }
 
-
-
-
-class Parser
-{
-private:
-	struct Attribute
-	{
-		std::string name;
-		std::string value;
-	};
-	struct Tag
-	{
-		int id;
-		int nest_id;
-		std::string type;
-		std::list<Attribute*>* attributes;
-		char content [MAX_BUF_SIZE];
-		int content_size = 0;
-	};
-
-	int PRINT_WIDTH = 10;
-	bool debug = false;
-	const char * input;
-	std::list<Tag*> Tag_List;
-	std::stack<Tag*> Tag_Stack;
-	int c = 0;
-	bool in_tag = false;
-	int global_id = 1;
-
-	void skip_spaces ()
-	{
-		// Skip space (32), tab (9), newline
-		if(debug) print_section("skip_spaces(), before loop");
-		while (input[c] == 32 || input[c] == 9 || input[c] == '\n')
-		{
-			if(debug) print_section("skip_spaces(), loop");
-			c++;
-		}
-	}
-	std::string open_tag ()
-	{
-		if(debug) print_section("open_tag() begin");
-		std::string tag_type = "";
-		std::string value;
-		int c_ = c;
-		if (input[c_] != '<')
-		{
-			if(debug) print_section("open_tag() did not see <");
-			return "";
-		}
-		if (input[c_ + 1] == '/')
-		{
-			if(debug) print_section("open_tag() found a close_tag");
-			return "";
-		}
-		c_++;
-		while (input[c_] != '\n' && input[c_] != '\0' && input[c_] != '>' && input[c_] != '/' && input[c_] != ' ')
-		{
-			if (input[c_] < 33 || (input[c_] > 57 && input[c_] < 65) || input[c_] > 122)
-			{
-				return "";
-			}
-			value = input[c_];
-			if(debug) print_section("open_tag() sees \"" + char_string(c_) + "\"");
-			tag_type += input[c_++];
-		}
-		c = c_;
-		return tag_type;
-	}
-
-	void get_attribute (Tag &tag)
-	{
-		Attribute *attrib = new Attribute ();
-		if(debug) print_section("get_attribute() begin");
-		while (input[c] != ' ' && input[c] != '=' && input[c] != '>')
-		{
-			attrib->name += input[c++];
-		}
-		if(debug) print_section("get_attribute() found \"" + attrib->name + "\"");
-		skip_spaces ();
-		if (input[c] != '=')
-		{
-
-		}
-		else
-		{
-			if(debug) print_section("get_attribute() saw =");
-			c++;
-			skip_spaces ();
-//			if (input[c] != '"' && input[c] != '\'')
-//			{
-//				fprintf(stderr, "Error, incorrectly formed attribute.\n");
-//				exit(1);
-//			}
-			char delimiter = 0; //= input[c];
-			if (input[c] == '"' || input[c] == '\'')
-			{
-				delimiter = input[c];
-				if(debug) print_section("get_attribute() saw delimiter " + char_string(c));
-				c++;
-			}
-			while (input[c] != '\0')
-			{
-				if (delimiter != 0)
-				{
-					if (input[c] == delimiter) break;
-				}
-				else
-				{
-					if (input[c] == ' ') break;
-				}
-				//if(debug) print_section("get_attribute() value += \"" + char_string(c) + "\"");
-				attrib->value += input[c++];
-			}
-			c++;
-		}
-		if(debug) print_section("get_attribute() stored \"" + attrib->name + "\" = \"" + attrib->value + "\"");
-		tag.attributes->push_back(attrib);
-	}
-
-	// We are in a tag, now we need to fill in the  Tag struct.
-	// We will stop filling in when we get to the end of the
-	// end of the tag .../> or ...> or EOF
-	void process_tag (std::string tag_type)
-	{
-		Tag tag;
-		tag.type = tag_type;
-		tag.id = global_id++;
-		tag.nest_id = Tag_Stack.size() == 0 ? 0 : Tag_Stack.top().id;
-		tag.attributes = new std::list<Attribute*> ();
-
-		if(debug) print_section("process_tag() begin");
-		Tag_List.push_back(tag);
-		Tag_Stack.push(tag);
-
-		if(tag_type == "!--")
-		{
-			if (debug) print_section("process_tag() defers to process_comment()");
-			process_comment ();
-			return;
-		}
-
-		// You have the tag, skip white space
-		if(debug) print_section("process_tag() skip_spaces");
-		skip_spaces ();
-
-		while (input[c] != '/' && input[c] != '>' && input[c] != '\0')
-		{
-			get_attribute(tag);
-			skip_spaces ();
-		}
-
-		// Exit the tag definition?
-		if (input[c] == '>')
-		{
-			c++;
-			return;
-		}
-
-		// Close the tag
-		if (input[c] == '/' && input[c + 1] == '>')
-		{
-			c = c + 2;
-			if(debug) print_section("process_tag() closed " + Tag_Stack.top().type);
-			Tag_Stack.pop();
-		}
-	}
-
-	std::string close_tag ()
-	{
-		if(debug) print_section("close_tag() begin");
-		std::string tag_name;
-		if (input[c] != '<' && input[c + 1] != '/') return "";
-		if(debug) print_section("close_tag() found ...");
-
-		c = c + 2;
-		while (input[c] != '>')
-		{
-			tag_name += input[c++];
-		}
-		if(debug) print_section("close_tag() found \"" + tag_name + "\"");
-
-		// Check if this should be within a script or terminate the script
-		Tag tag = Tag_Stack.top();
-		if ((tag_name == "script" || tag_name == "Script" || tag_name == "SCRIPT") && (tag.type == "script" || tag.type == "Script" || tag.type == "SCRIPT"))
-		{
-			return "";
-		}
-		c++;
-		if (tag.type == tag_name)
-		{
-			Tag_Stack.pop();
-		}
-		else
-		{
-			print_section ("error: close tag \"" + tag_name + "\" isn't matched.");
-		}
-		return tag_name;
-	}
-
-	std::string process_content ()
-	{
-		Tag tag = Tag_Stack.top();
-		if(debug) print_section("process_content() begin");
-		while (input[c] != '\0')
-		{
-			if (input[c] == '<') // && !(tag.type == "script" || tag.type == "Script" || tag.type == "SCRIPT"))
-			{
-				if(debug) print_section("process_content() break");
-				break;
-			}
-			//if(debug) print_section("process_content() for " + Tag_Stack.top().type);
-			tag.content[tag.content_size++] = input[c++];
-		}
-		tag.content[tag.content_size] = 0;
-		if(debug) print_section("process_content() for " + tag.type + " found: " + std::string(tag.content, tag.content + 1));
-		return tag.content;
-	}
-
-	void process_comment ()
-	{
-
-		if(debug) print_section ("process_comment() begin");
-		if (Tag_Stack.top().type != "!--")
-		{
-			if(debug) print_section ("process_comment() Error: top is \"" + Tag_Stack.top().type + "\"");
-			fprintf(stderr, "Error, comment tag is not Tag_Stack.top().\n");
-			exit(1);
-		}
-		Tag tag = Tag_Stack.top();
-		while (input[c] != '\0')
-		{
-			if (input[c] == '-' && input[c] == '-' && input[c] =='>')
-			{
-				c = c + 3;
-				if(debug) print_section("process_comment() found comment: " + std::string (tag.content, tag.content + 1));
-				Tag_Stack.pop();
-			}
-			tag.content[tag.content_size] += input[c++];
-			tag.content_size = tag.content_size + 1;
-			tag.content[tag.content_size] = 0;
-		}
-	}
-	void process ()
-	{
-		std::string tag_type;
-		while (input[c] != '\0')
-		{
-			skip_spaces(); if(debug) print_section("process() skip_spaces");
-			tag_type = open_tag (); if(debug) print_section("process() open_tag found: \"" + tag_type + "\"");
-			if(tag_type != "")
-			{
-				process_tag (tag_type);
-				process_content();
-				std::cout << "process(), Tag_Stack.top() " << Tag_Stack.top().content << std::endl;
-			}
-			else
-			{
-				tag_type = close_tag ();
-				if (tag_type == "")
-				{
-					process_content();
-					//std::cout << "process() sees: " << result;
-					std::cout << "process(), Tag_Stack.top() " << Tag_Stack.top().content << std::endl;
-				}
-			}
-		}
-	}
-
-	void print_section (std::string comment)
-	{
-		for (int i = c; input[i] != '\0' && i < (c + PRINT_WIDTH); i++)
-		{
-			std::cout << char_string(i);
-		}
-		std::cout << "      " << comment << std::endl;
-	}
-
-	std::string char_string (int i)
-	{
-		std::string str;
-		if (input[i] < 32)
-		{
-			str = "|";
-			str += (int)input[i];
-			str += "|";
-		}
-		else
-		{
-			str = std::string(1, input[i]);
-		}
-		return str;
-	}
-
-public:
-	Parser (const char *input_)
-	{
-			input = input_;
-	}
-	void set_debug (bool show_debug)
-	{
-		debug = show_debug;
-	}
-	int get_urls ()
-	{
-		if(debug) std::cout << "get_urls\n";
-		process ();
-	}
-	void print ()
-	{
-		for (std::list<Tag>::iterator it = Tag_List.begin(); it != Tag_List.end(); it++)
-		{
-			std::cout << it->id << "," << it->nest_id << "  " << it->type << std::endl;
-			for (std::list<Attribute*>::iterator it_attr = it->attributes->begin(); it_attr != it->attributes->end(); it_attr++)
-			{
-				std::cout << "     " << (*it_attr)->name << " = " << (*it_attr)->value << std::endl;
-			}
-			std::cout << "     content (" << it->content_size << ") = \"" << std::string(it->content, it->content + 1) << "\"" << std::endl;
-		}
-	}
-	std::list<std::string>* get_attribute_values (std::string attrib)
-	{
-
-	}
-};
-
 static size_t write_header (void *ptr, size_t size, size_t nmemb, void *data)
 {
 	int written = fwrite(ptr, size, nmemb, ((struct UrlData *)data)->file);
@@ -477,11 +131,6 @@ static size_t write_body (void *ptr, size_t size, size_t nmemb, void *data)
 
 int mCurl (const char* source_url, int level)
 {
-	//Parser Parser_((char *)ptr, (struct UrlData *)data);
-	//Parser_.set_debug(true);
-	//Parser_.get_urls();
-	//Parser_.print();
-
 	struct UrlData header_data;
 	struct UrlData body_data;
 
@@ -490,13 +139,17 @@ int mCurl (const char* source_url, int level)
 	std::string headerFilename = DATA_DIR + std::to_string(level) + "_" + std::string(source_url) + "_head.out";
 	std::string bodyFilename = DATA_DIR + std::to_string(level) + "_" + std::string(source_url) + "_body.out";
 	
+	std::list<std::string> *new_URLS;
+	std::unordered_set<std::string> *target_URLS;
+
 	try
 	{
-		std::unordered_set<std::string> *target_urls = URLS.at(source_url);
+		URLS.at(source_url);
 	}
 	catch (std::exception& e)
 	{
-		URLS.insert({{source_url, new std::unordered_set<std::string> ()}});
+		target_URLS = new std::unordered_set<std::string> ();
+		URLS.insert({{source_url, target_URLS}});
 	}
 
 	curl_global_init(CURL_GLOBAL_ALL);
@@ -552,9 +205,15 @@ int mCurl (const char* source_url, int level)
 		curl_easy_cleanup(curl_handle);
 		
 		Parser Parser_((char *)body_data.buffer);
-		Parser_.set_debug(true);
-		Parser_.get_urls();
-		Parser_.print();
+		Parser_.set_debug(false);
+		Parser_.process();
+
+		new_URLS = Parser_.get_attribute_values("href");
+
+		for (std::list<std::string>::iterator it=new_URLS->begin(); it != new_URLS->end(); it++)
+		{
+			target_URLS->insert(*it);
+		}
 
 		std::string footer = "[+] " + std::to_string(URLS.at(source_url)->size()) + " urls found.";
 		print_indent(level * INDENT_STEP); print_column (footer.c_str(), COLUMN_WIDTH);
